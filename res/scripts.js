318960cd6f4dba7e959cbe794fae7909bf0402f9
Peter Bubeliny <neri@neridev.com>
2018-04-01 19:30:36 +0200

    Refactoring

--- res/scripts.js
+++ res/scripts.js
@@ -6,45 +6,34 @@
 
 (() => {
 
-  const tematicke1 = 0.90;
-  const tematicke2 = 0.75;
-  const tematicke3 = 0.50;
-  const tematicke4 = 0.30;
+  const tematicke = ["0.90", "0.75", "0.50", "0.30"];
+  const priebezne = ["0.90", "0.75", "0.60", "0.46"];
 
-  const priebezne1 = 0.90;
-  const priebezne2 = 0.75;
-  const priebezne3 = 0.60;
-  const priebezne4 = 0.46;
+  let tematicPointsFull, continuPointsFull = 0;
 
-  let tematicPoints, tematicPoints1, tematicPoints2, tematicPoints3, tematicPoints4 = 0;
-  let continuPoints, continuPoints1, continuPoints2, continuPoints3, continuPoints4 = 0;
+  let tematicPoints = ["tematicPoints0", "tematicPoints1", "tematicPoints2", "tematicPoints3"];
+  let continuPoints = ["continuPoints0", "continuPoints1", "continuPoints2", "continuPoints3"];
 
   function tematicCalculate() {  
-    tematicPoints = document.getElementById('tematic').value;
-    //console.log('Loeaded tematic points: '+tematicPoints);
+    tematicPointsFull = document.getElementById('tematic').value;
+    //console.log('Loeaded tematic points: '+tematicPointsFull);
 
-    tematicPoints1 = tematicPoints * tematicke1;
-    document.getElementById('tematic1').value = Math.round(tematicPoints1);
-    tematicPoints2 = tematicPoints * tematicke2;
-    document.getElementById('tematic2').value = Math.round(tematicPoints2);
-    tematicPoints3 = tematicPoints * tematicke3;
-    document.getElementById('tematic3').value = Math.round(tematicPoints3);
-    tematicPoints4 = tematicPoints * tematicke4;
-    document.getElementById('tematic4').value = Math.round(tematicPoints4);
+    for (var i=0; i <4 ; i++) {
+      tematicPoints[i] = tematicPointsFull * tematicke[i];
+      document.getElementById('tematic'+i).value = Math.round(tematicPoints[i]);
+      //console.log('Tematic '+i+': '+tematicPoints[i]);
+    }
   }
 
   function continuCalculate() {  
-    continuPoints = document.getElementById('continu').value;
-    //console.log('Loeaded continu points: '+continuPoints);
+    continuPointsFull = document.getElementById('continu').value;
+    //console.log('Loeaded continu points: '+continuPointsFull);
 
-    continuPoints1 = continuPoints * priebezne1;
-    document.getElementById('continu1').value = Math.round(continuPoints1);
-    continuPoints2 = continuPoints * priebezne2;
-    document.getElementById('continu2').value = Math.round(continuPoints2);
-    continuPoints3 = continuPoints * priebezne3;
-    document.getElementById('continu3').value = Math.round(continuPoints3);
-    continuPoints4 = continuPoints * priebezne4;
-    document.getElementById('continu4').value = Math.round(continuPoints4);
+    for (var i=0; i <4 ; i++) {
+      continuPoints[i] = continuPointsFull * priebezne[i];
+      document.getElementById('continu'+i).value = Math.round(continuPoints[i]);
+      //console.log('Tematic '+i+': '+continuPoints[i]);
+    }
   }
 
   document.getElementById('tematicCalculate').addEventListener('click', () => {
